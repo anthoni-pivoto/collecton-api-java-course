e0789943c38a0812363677b02e05aeeccb02c95f
anthoni-pivoto <anthonipivoto@gmail.com>
2023-10-14 18:40:08 -0300

    Aprendendo e exercitando Set<>
    Signed-off-by: anthoni-pivoto <anthonipivoto@gmail.com>

---
+++ src/set/ordenar/CadastroProdutos.java
@@ -0,0 +1,28 @@
+package set.ordenar;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preço, int quantidade){
+        produtoSet.add(new Produto(codigo, nome, preço, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreço(){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+}
---
+++ src/set/ordenar/Produto.java
@@ -0,0 +1,70 @@
+package set.ordenar;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto> {
+    private long codigo;
+    private String nome;
+    private double preço;
+    private int quantidade;
+
+    public Produto(long codigo, String nome, double preço, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preço = preço;
+        this.quantidade = quantidade;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return getCodigo() == produto.getCodigo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", preço=" + preço +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Produto>{
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreço(), p2.getPreço());
+    }
+}
